0e2a5c5d13d6da8e1541300300e6f0691b13acb0
JKGzenna <juanlu360@gmail.com>
2018-07-12 01:10:07 +0200

    INICIAL

---
+++ src/test/java/PersonaTest.java
@@ -0,0 +1,28 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PersonaTest {
+	
+	private Persona persona;
+
+	@Before
+	public void setUp() throws Exception {
+		persona = new Persona ("Juan", "Goldaracena");
+		System.out.println("ANTES DE CADA TEST");
+	}
+
+	@Test
+	public void testGetNombre() throws Exception {
+		//given  //condiciones previas
+
+		//when  //ejecucion del método a probar
+
+		//then  //condiciones que comprobamos que se cumplan para validar el test
+		Assert.assertTrue(persona.getNombre().equals("Juan"));
+		System.out.println("HACEMOS EL TEST");
+		
+		
+	}
+
+}
